7e2f6f56aac3f71e14b902eb96a2a49f47aa70dd
EC2 Default User <ec2-user@ip-172-31-9-69.ap-northeast-1.compute.internal>
2024-03-06 01:40:30 +0000

    9-4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_05_123455) do
+ActiveRecord::Schema.define(version: 2024_03_06_011436) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 2024_03_05_123455) do
   create_table "book_comments", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "book_id"
+    t.integer "user_id"
+    t.text "comment"
   end
 
   create_table "books", force: :cascade do |t|
@@ -56,6 +59,8 @@ ActiveRecord::Schema.define(version: 2024_03_05_123455) do
   create_table "favorites", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "book_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
