3350422671befca50deb7aae878ea14e4dc08fbb
EC2 Default User <ec2-user@ip-172-31-9-69.ap-northeast-1.compute.internal>
2024-03-06 08:09:14 +0000

    9-7

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,14 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
   # フォローするとき
   def create
+    user = User.find(params[:user_id])
     current_user.follow(params[:user_id])
     redirect_to request.referer
   end
   # フォロー外すとき
   def destroy
+    user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
